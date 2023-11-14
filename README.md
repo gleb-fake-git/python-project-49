a957bfaa9bad73b9c17f8b96165c4f3020e2a0a3
Gleb Gavrilov <y1@bk.ru>
2023-11-15 00:56:12 +0300

    add new game progression

--- README.md
+++ README.md
@@ -6,7 +6,10 @@ brain-even:
 [![asciicast](https://asciinema.org/a/XbdDtY6qsL4tfiDb5X4ZahZV1.png)](https://asciinema.org/a/XbdDtY6qsL4tfiDb5X4ZahZV1)
 
 brain-calc:
-[![asciicast](https://asciinema.org/a/XbdDtY6qsL4tfiDb5X4ZahZV1.png)](https://asciinema.org/a/pjIJAz4oCWNRySSPTrisspyib)
+[![asciicast](https://asciinema.org/a/pjIJAz4oCWNRySSPTrisspyib.png)](https://asciinema.org/a/pjIJAz4oCWNRySSPTrisspyib)
 
 brain-gcd:
-[![asciicast](https://asciinema.org/a/XbdDtY6qsL4tfiDb5X4ZahZV1.png)](https://asciinema.org/a/aqNCXXMD5Zz9WlL76TS1cQJ99)
+[![asciicast](https://asciinema.org/a/aqNCXXMD5Zz9WlL76TS1cQJ99.png)](https://asciinema.org/a/aqNCXXMD5Zz9WlL76TS1cQJ99)
+
+brain-progression:
+[![asciicast](https://asciinema.org/a/iUjICMryhVGXAyEWyUm6oCl1e.png)](https://asciinema.org/a/iUjICMryhVGXAyEWyUm6oCl1e)
---
+++ brain_games/scripts/games/brain_progression.py
@@ -0,0 +1,47 @@
+from random import randint, choice
+from brain_games.game_engine import welcome_user, get_user_answer, message_lose
+from brain_games.game_engine import GAME_SETTINGS
+
+
+def generate_task():
+    step = randint(2, 15)
+    len_progression = randint(5, 10)
+    hidden_index = randint(0, len_progression - 1)
+    number = randint(1, 50)
+    right_answer = 0
+    text_answer = []
+    progression = []
+    while len_progression != 0:
+        progression.append(number)
+        number += step
+        len_progression -= 1
+    for index, value in enumerate(progression):
+        if index == hidden_index:
+            text_answer.append('..')
+            right_answer = value
+        else:
+            text_answer.append(str(value))
+    return right_answer, ' '.join(text_answer)
+
+
+def main():
+    user_name = welcome_user()
+    print('What number is missing in the progression?')
+    attempts = GAME_SETTINGS['game_attempts']
+    while attempts > 0:
+        right_answer, text_answer = generate_task()
+        print(f'Question: {text_answer}')
+        user_message = int(get_user_answer())
+        if user_message == right_answer:
+            print('Correct!')
+        else:
+            message_lose(right_answer, user_message, user_name)
+            break
+        attempts -= 1
+        if attempts == 0:
+            print(f'Congratulations, {user_name}!')
+            break
+
+
+if __name__ == '__main__':
+    main()
