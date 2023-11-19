c738cbaed4a7fefddb9dc02543ede9ba25cf1ce2
Gleb Gavrilov <y1@bk.ru>
2023-11-19 22:33:35 +0300

    refactor: new generate algorithm for progression

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -5,22 +5,12 @@ from brain_games.game_engine import start_game
 def generate_task():
     step = randint(2, 15)
     len_progression = randint(5, 10)
-    hidden_index = randint(0, len_progression - 1)
     number = randint(1, 50)
-    right_answer = 0
-    text_answer = []
-    progression = []
-    while len_progression != 0:
-        progression.append(number)
-        number += step
-        len_progression -= 1
-    for index, value in enumerate(progression):
-        if index == hidden_index:
-            text_answer.append('..')
-            right_answer = value
-        else:
-            text_answer.append(str(value))
-    return str(right_answer), ' '.join(text_answer)
+    stop = (((len_progression * step) + number) - step) + 1
+    progression = list(map(str, range(number, stop, step)))
+    missed_index = randint(0, len(progression) - 1)
+    progression[missed_index], right_answer = '..', str(progression[missed_index])
+    return str(right_answer), ' '.join(progression)
 
 
 def main():
